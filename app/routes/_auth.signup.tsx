75f0095fe63799a033e6ad2c17d66d63b787c994
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-07-11 15:27:40 -0400

    Add signup route

---
+++ app/routes/_auth.signup.tsx
@@ -0,0 +1,9 @@
+import SignupForm from "~/components/auth/signupForm";
+
+export default function CreateAccount() {
+  return (
+    <div className="font-sans p-4">
+      <SignupForm />
+    </div>
+  );
+}
