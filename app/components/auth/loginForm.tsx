aaf6212c5cf1ce3ef6d2583072007f6f0ebd9322
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-07-10 23:33:52 -0400

    Add a form with email and password inputs

--- app/components/auth/loginForm.tsx
+++ app/components/auth/loginForm.tsx
@@ -9,26 +9,82 @@ import {
 import { z } from "zod";
 import { loginFormSchema } from "~/lib/zodSchemas/loginFormSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
-
-const loginForm = useForm<z.infer<typeof loginFormSchema>>({
-  resolver: zodResolver(loginFormSchema),
-  defaultValues: {
-    username: "",
-  },
-});
-
-const onSubmit = (values: z.infer<typeof loginFormSchema>) => {
-  console.log(values);
-};
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
 
 export default function LoginForm() {
+  const loginForm = useForm<z.infer<typeof loginFormSchema>>({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof loginFormSchema>) => {
+    console.log(values);
+  };
+
   return (
     <Card>
       <CardHeader>
         <CardTitle>Welcome to Munchlify!</CardTitle>
         <CardDescription>Existing users can log in below.</CardDescription>
-        <CardContent></CardContent>
       </CardHeader>
+      <CardContent>
+        <Form {...loginForm}>
+          <form
+            onSubmit={loginForm.handleSubmit(onSubmit)}
+            className="space-y-4"
+          >
+            <FormField
+              control={loginForm.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="email@host.com"
+                      autoComplete="username"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={loginForm.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="********"
+                      autoComplete="current-password"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Log In</Button>
+          </form>
+        </Form>
+      </CardContent>
     </Card>
   );
 }
