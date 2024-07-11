7d8bc534becd6daf4578f83a669385ed508c6dc4
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-07-11 19:46:13 -0400

    Make body hfull with basic flex layout

--- app/root.tsx
+++ app/root.tsx
@@ -10,14 +10,14 @@ import MainLayout from "./components/mainLayout";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full m-0">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="h-full m-0 flex flex-col">
         {children}
         <ScrollRestoration />
         <Scripts />
