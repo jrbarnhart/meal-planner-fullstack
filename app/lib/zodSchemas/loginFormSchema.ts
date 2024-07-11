534c99e40552bd8cfba04b59d610e3776520dde3
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-07-10 23:04:41 -0400

    Change username to email

--- app/lib/zodSchemas/loginFormSchema.ts
+++ app/lib/zodSchemas/loginFormSchema.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
 export const loginFormSchema = z.object({
-  username: z.string().trim().email().max(254),
+  email: z.string().trim().email().max(254),
   password: z.string().trim(),
 });
