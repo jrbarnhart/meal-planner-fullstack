2b538a8c049d2cbe7df8bc1271adddb47c2be30e
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-07-11 15:13:36 -0400

    Add schema for signup form

--- app/lib/zodSchemas/loginFormSchema.ts
+++ app/lib/zodSchemas/loginFormSchema.ts
@@ -1,6 +1,31 @@
 import { z } from "zod";
 
+const emailSchema = z
+  .string()
+  .trim()
+  .email({
+    message: "Must be a valid email format. Example: user@service.com",
+  })
+  .min(5, { message: "Email must be at least 5 characters long" })
+  .max(320, { message: "Email must be at most 320 characters long" });
+
 export const loginFormSchema = z.object({
-  email: z.string().trim().email().max(254),
+  email: emailSchema,
   password: z.string().trim(),
+});
+
+export const signupFormSchema = z.object({
+  email: emailSchema,
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long" })
+    .max(100, { message: "Password must be at most 100 characters long" })
+    .regex(/[A-Z]/, {
+      message: "Password must contain at least one uppercase letter",
+    })
+    .regex(/[a-z]/, {
+      message: "Password must contain at least one lowercase letter",
+    })
+    .regex(/[0-9]/, { message: "Password must contain at least one number" })
+    .regex(/^\S*$/, { message: "Password must not contain spaces" }),
 });
