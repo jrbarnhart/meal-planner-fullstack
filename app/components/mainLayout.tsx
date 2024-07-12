3ad59d7b002292205cb3ceb4be1758829af668e3
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-07-12 14:04:30 -0400

    Fix button size and link to meals not /

--- app/components/mainLayout.tsx
+++ app/components/mainLayout.tsx
@@ -11,14 +11,12 @@ function Nav() {
   return (
     <Card
       className={`absolute bottom-1/4 right-0 p-2 bg-card transition-all duration-300 ease-in-out rounded-r-none ${
-        isOpen ? "w-48" : "w-16"
+        isOpen ? "w-48" : "w-20"
       }`}
     >
       <nav className="flex items-center gap-2">
         <Button
-          className={`h-28 flex-shrink-0 ${
-            isOpen ? "w-12" : "w-16"
-          } transition-all duration-300 ease-in-out`}
+          className={`h-28 flex-shrink-0 w-14`}
           onMouseDown={() => setIsOpen((prev) => !prev)}
         >
           {isOpen ? ">" : "<"}
@@ -28,14 +26,14 @@ function Nav() {
             isOpen ? "w-44 opacity-100" : "w-0 opacity-0"
           }`}
         >
-          <Link to={pathname.startsWith("/recipes") ? "/" : "/recipes"}>
+          <Link to={pathname.startsWith("/recipes") ? "/meals" : "/recipes"}>
             <Button className="w-full">
-              {pathname.startsWith("/recipes") ? "Home" : "Recipes"}
+              {pathname.startsWith("/recipes") ? "Meals" : "Recipes"}
             </Button>
           </Link>
-          <Link to={pathname.startsWith("/shopping") ? "/" : "/shopping"}>
+          <Link to={pathname.startsWith("/shopping") ? "/meals" : "/shopping"}>
             <Button className="w-full">
-              {pathname.startsWith("/shopping") ? "Home" : "Shopping"}
+              {pathname.startsWith("/shopping") ? "Meals" : "Shopping"}
             </Button>
           </Link>
         </div>
