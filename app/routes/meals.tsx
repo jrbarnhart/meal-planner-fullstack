367712646b1db870fd748985455b59c9d9e1183e
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-07-12 17:04:57 -0400

    Add simple shadcn carousel

--- app/routes/meals.tsx
+++ app/routes/meals.tsx
@@ -1,7 +1,36 @@
+import { Card, CardContent } from "~/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "~/components/ui/carousel";
+
 export default function Meals() {
   return (
-    <div>
-      <p>This is the meals page!</p>
-    </div>
+    // Month Name Date Picker
+    // Fast Day Carousel
+    // Detail Day Carousel
+    // Meals Interface
+    <>
+      <Carousel className="w-full max-w-xs">
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index}>
+              <div className="p-1">
+                <Card>
+                  <CardContent className="flex aspect-square items-center justify-center p-6">
+                    <span className="text-4xl font-semibold">{index + 1}</span>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </>
   );
 }
