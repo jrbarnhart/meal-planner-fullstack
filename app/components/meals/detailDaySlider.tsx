bc9117bec0539479f309eba633a8bd3c34c347fc
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-07-15 13:19:27 -0400

    Add description to cards

--- app/components/meals/detailDaySlider.tsx
+++ app/components/meals/detailDaySlider.tsx
@@ -1,4 +1,10 @@
-import { Card, CardContent, CardHeader } from "../ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
 import {
   Carousel,
   CarouselContent,
@@ -28,17 +34,23 @@ export default function DetailDaySlider() {
             <div className="p-1">
               <Card className="h-[188px]">
                 <CardHeader className="flex items-center justify-center p-0">
-                  <p className="text-lg font-semibold">
-                    {index + 1 >= 10 && index + 1 <= 13
-                      ? `${index + 1}th`
-                      : (index + 1).toString().endsWith("1")
-                      ? `${index + 1}st`
-                      : (index + 1).toString().endsWith("2")
-                      ? `${index + 1}nd`
-                      : (index + 1).toString().endsWith("3")
-                      ? `${index + 1}rd`
-                      : `${index + 1}th`}
-                  </p>
+                  <CardTitle>
+                    <p className="text-lg font-semibold">
+                      {index + 1 >= 10 && index + 1 <= 13
+                        ? `${index + 1}th`
+                        : (index + 1).toString().endsWith("1")
+                        ? `${index + 1}st`
+                        : (index + 1).toString().endsWith("2")
+                        ? `${index + 1}nd`
+                        : (index + 1).toString().endsWith("3")
+                        ? `${index + 1}rd`
+                        : `${index + 1}th`}
+                    </p>
+                  </CardTitle>
+                  <CardDescription className="w-full flex justify-between px-2">
+                    <span>Recipe</span>
+                    <span>Shopped</span>
+                  </CardDescription>
                 </CardHeader>
                 <CardContent className="grid grid-flow-col gap-2 px-2">
                   <SmallRecipeEntry />
