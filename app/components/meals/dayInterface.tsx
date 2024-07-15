916dfbfec74b8f875a14a106870b447c7ba3c5a0
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-07-15 16:33:13 -0400

    Add description that desribes entries

--- app/components/meals/dayInterface.tsx
+++ app/components/meals/dayInterface.tsx
@@ -1,11 +1,11 @@
 import { Button } from "../ui/button";
-import { Card, CardContent, CardHeader } from "../ui/card";
+import { Card, CardContent, CardDescription, CardHeader } from "../ui/card";
 import { Separator } from "../ui/separator";
 
 function MealEntry() {
   return (
-    <div className="flex justify-between items-center">
-      <p>Recipe Name</p>
+    <div className="grid grid-flow-col grid-cols-[4fr_3fr_2fr_1fr] items-center text-nowrap">
+      <p className="truncate">Recipe Name</p>
       <p>00:00</p>
       <p>4</p>
       <Button>...</Button>
@@ -18,9 +18,15 @@ export default function DayInterface() {
     <Card className="p-2 w-full overflow-hidden">
       <CardHeader className="p-1 text-lg font-semibold flex items-center">
         <p>Date</p>
+        <CardDescription className="grid grid-flow-col grid-cols-[4fr_3fr_2fr_1fr] w-full">
+          <span>Recipe Name</span>
+          <span>Prep Time</span>
+          <span>Feeds</span>
+          <span>Actions</span>
+        </CardDescription>
       </CardHeader>
       <Separator />
-      <CardContent className="p-2 pb-8 grid gap-y-2 overflow-y-scroll h-full">
+      <CardContent className="p-2 pb-16 grid gap-y-2 overflow-y-scroll h-full">
         <MealEntry />
         <MealEntry />
         <MealEntry />
