be13ff67392588ee08daccd1f75aacafe1b75c52
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-07-16 15:37:30 -0400

    Add custom hook for getting screen size

---
+++ app/lib/hooks/useScreenSize.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+export default function useScreenSize() {
+  const [screenSize, setScreenSize] = useState({
+    width: typeof window !== "undefined" ? window.innerWidth : 0,
+    height: typeof window !== "undefined" ? window.innerHeight : 0,
+  });
+
+  useEffect(() => {
+    if (typeof window === "undefined") return;
+
+    const handleResize = () => {
+      setScreenSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return screenSize;
+}
