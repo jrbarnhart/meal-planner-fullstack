8a59e207e238a769c113dec31b1c0ee2bb020265
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-07-20 17:32:50 -0400

    Log all the types in a types property on values

--- app/routes/recipes.add.tsx
+++ app/routes/recipes.add.tsx
@@ -2,7 +2,7 @@ import { ActionFunctionArgs } from "@remix-run/node";
 import { Form, Link } from "@remix-run/react";
 import RouteContent from "~/components/layout/routeContent";
 import { Button } from "~/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
+import { Card, CardContent } from "~/components/ui/card";
 import { Checkbox } from "~/components/ui/checkbox";
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
@@ -11,23 +11,30 @@ import { Textarea } from "~/components/ui/textarea";
 export async function action({ request }: ActionFunctionArgs) {
   const formData = await request.formData();
 
-  const values = Object.fromEntries(formData);
+  const values: Record<string, FormDataEntryValue | FormDataEntryValue[]> = {};
+
+  for (const [key, value] of formData.entries()) {
+    if (key === "types[]") {
+      values["types"] = formData.getAll("types[]");
+    } else {
+      values[key] = value;
+    }
+  }
 
   console.log(values);
+  return null;
 }
 
 export default function CreateRecipe() {
   return (
     <RouteContent>
-      <div className="w-full">
+      <div className="w-full flex justify-between">
         <Button asChild>
           <Link to={"/recipes"}>My Recipes</Link>
         </Button>
+        <h1 className="text-xl">Add a New Recipe</h1>
       </div>
-      <Card className="w-full">
-        <CardHeader>
-          <CardTitle>Add a New Recipe</CardTitle>
-        </CardHeader>
+      <Card className="w-full overflow-y-auto">
         <CardContent>
           <Form method="post" className="space-y-4">
             <div>
@@ -53,41 +60,51 @@ export default function CreateRecipe() {
               ></Input>
             </div>
             <div>
+              <Label htmlFor="feeds">Feeds</Label>
+              <Input
+                id="feeds"
+                name="feeds"
+                type="number"
+                min={1}
+                placeholder="1"
+              ></Input>
+            </div>
+            <div>
               <Label htmlFor="types">Type(s):</Label>
               <Card className="p-2">
                 <fieldset id="types" className="grid grid-cols-2 gap-4">
                   <div className="flex items-center gap-2">
-                    <Checkbox id="t1" name="breakfast" />
+                    <Checkbox id="t1" name="types[]" value="breakfast" />
                     <Label htmlFor="t1" className="text-lg">
                       Breakfast
                     </Label>
                   </div>
                   <div className="flex items-center gap-2">
-                    <Checkbox id="t2" name="lunch" />
+                    <Checkbox id="t2" name="types[]" value="lunch" />
                     <Label htmlFor="t2" className="text-lg">
                       Lunch
                     </Label>
                   </div>
                   <div className="flex items-center gap-2">
-                    <Checkbox id="t3" name="dinner" />
+                    <Checkbox id="t3" name="types[]" value="dinner" />
                     <Label htmlFor="t3" className="text-lg">
                       Dinner
                     </Label>
                   </div>
                   <div className="flex items-center gap-2">
-                    <Checkbox id="t4" name="dessert" />
+                    <Checkbox id="t4" name="types[]" value="dessert" />
                     <Label htmlFor="t4" className="text-lg">
                       Dessert
                     </Label>
                   </div>
                   <div className="flex items-center gap-2">
-                    <Checkbox id="t5" name="brunch" />
+                    <Checkbox id="t5" name="types[]" value="brunch" />
                     <Label htmlFor="t5" className="text-lg">
                       Brunch
                     </Label>
                   </div>
                   <div className="flex items-center gap-2">
-                    <Checkbox id="t6" name="snacks" />
+                    <Checkbox id="t6" name="types[]" value="snacks" />
                     <Label htmlFor="t6" className="text-lg">
                       Snacks
                     </Label>
@@ -95,6 +112,7 @@ export default function CreateRecipe() {
                 </fieldset>
               </Card>
             </div>
+            <Button type="submit">Add Recipe</Button>
           </Form>
         </CardContent>
       </Card>
