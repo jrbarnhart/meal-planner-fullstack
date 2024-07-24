d8a64ab44c7a1d204023e64af5f1be25ca3ac4c0
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-07-24 19:47:29 -0400

    Add util to delete recipe from storage

--- app/lib/localStorageUtils.ts
+++ app/lib/localStorageUtils.ts
@@ -27,3 +27,24 @@ export function addLocalRecipe(recipe: PHRecipe) {
     }
   }
 }
+
+export function deleteLocalRecipe(recipe: PHRecipe) {
+  const localRecipesString = localStorage.getItem("localRecipes");
+
+  if (!localRecipesString) {
+    return console.error("Cannot delete recipe. No local recipes found.");
+  }
+
+  const localRecipes = JSON.parse(localRecipesString);
+  const zodResults = recipeArraySchema.safeParse(localRecipes);
+  if (!zodResults.success) {
+    return console.error(
+      "Error parsing localRecipes from local storage. Incorrect data format."
+    );
+  }
+  const verifiedRecipes = zodResults.data;
+  const newLocalRecipes = verifiedRecipes.filter(
+    (verRec) => verRec.id !== recipe.id
+  );
+  localStorage.setItem("localRecipes", JSON.stringify(newLocalRecipes));
+}
