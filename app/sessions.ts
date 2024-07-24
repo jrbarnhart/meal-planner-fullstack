ba205e5ef1cf777b76084a2ebae84d0dda432d96
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-07-24 16:18:14 -0400

    Add sessions module based on remix docs

---
+++ app/sessions.ts
@@ -0,0 +1,24 @@
+import { createCookieSessionStorage } from "@remix-run/node";
+
+type SessionData = {
+  userId: string;
+};
+
+type SessionFlashData = {
+  error: string;
+};
+
+const { getSession, commitSession, destroySession } =
+  createCookieSessionStorage<SessionData, SessionFlashData>({
+    cookie: {
+      name: "__session",
+      httpOnly: true,
+      maxAge: 60,
+      path: "/",
+      sameSite: "lax",
+      secrets: [process.env.SESSION_SECRET || ""],
+      secure: process.env.NODE_ENV === "production",
+    },
+  });
+
+export { getSession, commitSession, destroySession };
