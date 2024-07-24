c961d8d3883162a1585e1377253f2ad40c473197
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-07-24 17:57:08 -0400

    Add zod schema for recipes

---
+++ app/lib/zodSchemas/recipeSchema.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const recipeSchema = z.object({
+  id: z.number().int().min(0),
+  name: z.string().trim().min(1).max(256),
+  description: z.string().trim(),
+  types: z.array(z.string().trim()),
+  time: z.number().int().min(1),
+  feeds: z.number().int().min(1),
+  requirements: z.array(z.string().trim()),
+  ingredients: z.array(z.string().trim()),
+  preNotes: z.string().trim(),
+  steps: z.array(z.string().trim()),
+  postNotes: z.string().trim(),
+});
