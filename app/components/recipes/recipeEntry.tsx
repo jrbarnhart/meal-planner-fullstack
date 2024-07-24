ba025f03db05cdeff9c92bcf6025cca939fc0afd
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-07-24 17:25:22 -0400

    Generate content based on passed recipe

--- app/components/recipes/recipeEntry.tsx
+++ app/components/recipes/recipeEntry.tsx
@@ -1,13 +1,28 @@
+import { Link } from "@remix-run/react";
 import { Button } from "../ui/button";
 import { Card } from "../ui/card";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { PHRecipe } from "~/lib/phData";
 
-export default function RecipeEntry() {
+export default function RecipeEntry({ recipe }: { recipe: PHRecipe }) {
+  const { id, name, time, feeds } = recipe;
   return (
     <Card className="grid grid-flow-col grid-cols-[4fr_3fr_2fr_1fr] items-center p-2">
-      <p>Name</p>
-      <p>00:00</p>
-      <p>4</p>
-      <Button variant={"secondary"}>...</Button>
+      <p className="truncate">{name[0].toUpperCase() + name.slice(1)}</p>
+      <p>{`${time} min`}</p>
+      <p>{feeds}</p>
+      <Popover>
+        <PopoverTrigger>
+          <Button variant={"secondary"}>...</Button>
+        </PopoverTrigger>
+        <PopoverContent className="flex flex-col gap-3 w-40">
+          <p>{name}</p>
+          <Button asChild>
+            <Link to={`/recipes/${id}`}>Details</Link>
+          </Button>
+          <Button variant={"destructive"}>Remove</Button>
+        </PopoverContent>
+      </Popover>
     </Card>
   );
 }
