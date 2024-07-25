71b3d12296753be03bcc75038cb2703dfac2522f
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-07-24 22:03:44 -0400

    Remove optional as at least an empty string always submitted

--- app/lib/zodSchemas/recipeSchema.ts
+++ app/lib/zodSchemas/recipeSchema.ts
@@ -19,7 +19,7 @@ export const addLocalRecipeSchema = z.object({
   // -int to prevent clashes with db ids
   id: z.number().int().max(-1),
   name: z.string().trim().min(1).max(256),
-  description: z.string().trim().optional(),
+  description: z.string().trim(),
   types: z.array(z.string().trim()),
   time: z
     .string()
@@ -31,9 +31,9 @@ export const addLocalRecipeSchema = z.object({
     .pipe(z.number().min(1)),
   requirements: z.array(z.string().trim()),
   ingredients: z.array(z.string().trim()),
-  preNotes: z.string().trim().optional(),
+  preNotes: z.string().trim(),
   steps: z.array(z.string().trim()),
-  postNotes: z.string().trim().optional(),
+  postNotes: z.string().trim(),
 });
 
 export const recipeArraySchema = z.array(localRecipeSchema);
