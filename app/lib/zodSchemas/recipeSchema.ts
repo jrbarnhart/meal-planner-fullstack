3f270ccfc3fe5dcb673ffe1470ec4e2ac142cadc
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-07-24 21:31:09 -0400

    Add schema for adding local recipe

--- app/lib/zodSchemas/recipeSchema.ts
+++ app/lib/zodSchemas/recipeSchema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const localRecipeSchema = z.object({
   // -int to prevent clashes with db ids
-  id: z.number().int().max(-1),
+  id: z.number().int(),
   name: z.string().trim().min(1).max(256),
   description: z.string().trim(),
   types: z.array(z.string().trim()),
@@ -13,6 +13,21 @@ export const localRecipeSchema = z.object({
   preNotes: z.string().trim(),
   steps: z.array(z.string().trim()),
   postNotes: z.string().trim(),
+});
+
+export const addLocalRecipeSchema = z.object({
+  // -int to prevent clashes with db ids
+  id: z.number().int().max(-1),
+  name: z.string().trim().min(1).max(256),
+  description: z.string().trim().optional(),
+  types: z.array(z.string().trim()),
+  time: z.number().int().min(1),
+  feeds: z.number().int().min(1),
+  requirements: z.array(z.string().trim()),
+  ingredients: z.array(z.string().trim()),
+  preNotes: z.string().trim().optional(),
+  steps: z.array(z.string().trim()),
+  postNotes: z.string().trim().optional(),
 });
 
 export const recipeArraySchema = z.array(localRecipeSchema);
