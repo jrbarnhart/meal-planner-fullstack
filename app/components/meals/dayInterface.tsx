66ec4cf65bb5af3aafd03aff563319e0308d3657
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-07-28 18:32:03 -0400

    Map selectedDate recipes to meal entries

--- app/components/meals/dayInterface.tsx
+++ app/components/meals/dayInterface.tsx
@@ -1,23 +1,31 @@
+import { formatDateForTitle } from "~/lib/utils";
 import { Button } from "../ui/button";
 import { Card, CardContent, CardDescription, CardHeader } from "../ui/card";
 import { Separator } from "../ui/separator";
+import { PHMealPlan, PHRecipe } from "~/lib/phData";
 
-function MealEntry() {
+function MealEntry({ recipe }: { recipe: PHRecipe }) {
   return (
     <div className="grid grid-flow-col grid-cols-[4fr_3fr_2fr_1fr] items-center text-nowrap">
-      <p className="truncate">Recipe Name</p>
-      <p>00:00</p>
-      <p>4</p>
+      <p className="truncate">{recipe.name}</p>
+      <p>{`${recipe.time} min`}</p>
+      <p>{`Feeds ${recipe.feeds}`}</p>
       <Button variant={"secondary"}>...</Button>
     </div>
   );
 }
 
-export default function DayInterface() {
+export default function DayInterface({
+  ...props
+}: {
+  selectedDate: Date;
+  currentMealPlans: PHMealPlan[];
+}) {
+  const { selectedDate, currentMealPlans } = props;
   return (
     <Card className="p-2 w-full overflow-hidden">
       <CardHeader className="p-1 text-lg font-semibold flex items-center">
-        <p>Date</p>
+        <p>{formatDateForTitle(selectedDate)}</p>
         <CardDescription className="grid grid-flow-col grid-cols-[4fr_3fr_2fr_1fr] w-full">
           <span>Recipe Name</span>
           <span>Prep Time</span>
@@ -27,12 +35,11 @@ export default function DayInterface() {
       </CardHeader>
       <Separator />
       <CardContent className="p-2 pb-16 grid gap-y-2 overflow-y-scroll h-full">
-        <MealEntry />
-        <MealEntry />
-        <MealEntry />
-        <MealEntry />
-        <MealEntry />
-        <MealEntry />
+        {currentMealPlans
+          .find((meal) => meal.date.getDate() === selectedDate.getDate())
+          ?.recipes.map((recipe, index) => (
+            <MealEntry recipe={recipe} key={index} />
+          ))}
         <Button className="w-36 h-12 my-4 justify-self-center">+</Button>
       </CardContent>
     </Card>
