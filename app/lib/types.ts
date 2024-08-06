c2fabc9dbe0a6a28f6ba2b6d777a8b41ec0f73f5
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-08-06 02:55:48 -0400

    Add prisma type for meal plan w/ recipes

--- app/lib/types.ts
+++ app/lib/types.ts
@@ -1,1 +1,11 @@
+import { Prisma } from "@prisma/client";
+
 export type ActionError = { [key: string]: string[] } | undefined;
+
+const mealPlanWithRecipes = Prisma.validator<Prisma.MealPlanDefaultArgs>()({
+  include: { recipes: true },
+});
+
+export type MealPlanWithRecipes = Prisma.MealPlanGetPayload<
+  typeof mealPlanWithRecipes
+>;
