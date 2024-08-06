8ff93a2de116d4c5ba25d04ce418022c5bf813cf
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-08-06 00:14:52 -0400

    Add addRecipeSchema

--- app/lib/zodSchemas/recipeSchema.ts
+++ app/lib/zodSchemas/recipeSchema.ts
@@ -34,4 +34,20 @@ export const addLocalRecipeSchema = z.object({
   isDefault: z.boolean(),
 });
 
+export const addRecipeSchema = z.object({
+  id: z.number().int(),
+  name: z.string().trim().min(1).max(256),
+  description: z.string().trim(),
+  types: z.array(z.string().trim()),
+  time: z.number().int().min(1),
+  feeds: z.number().int().min(1),
+  requirements: z.array(z.string().trim()),
+  ingredients: z.array(z.string().trim()),
+  preNotes: z.string().trim(),
+  steps: z.array(z.string().trim()),
+  postNotes: z.string().trim(),
+  userId: z.number().int(),
+  isDefault: z.boolean(),
+});
+
 export const recipeArraySchema = z.array(localRecipeSchema);
