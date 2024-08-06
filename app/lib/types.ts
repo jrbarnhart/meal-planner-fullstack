cae8dac65ced01c48c803450d891180cff909d90
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-08-06 03:20:54 -0400

    Remove meal plan type

--- app/lib/types.ts
+++ app/lib/types.ts
@@ -1,11 +1,1 @@
-import { Prisma } from "@prisma/client";
-
 export type ActionError = { [key: string]: string[] } | undefined;
-
-const mealPlanWithRecipes = Prisma.validator<Prisma.MealPlanDefaultArgs>()({
-  include: { recipes: true },
-});
-
-export type MealPlanWithRecipes = Prisma.MealPlanGetPayload<
-  typeof mealPlanWithRecipes
->;
