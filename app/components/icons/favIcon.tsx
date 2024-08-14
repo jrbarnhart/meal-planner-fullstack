f16abd30d4ed8f5839f4511b44ff543f1efa3f96
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-08-14 15:05:00 -0400

    Add favIcon component

---
+++ app/components/icons/favIcon.tsx
@@ -0,0 +1,370 @@
+export default function FavIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="64"
+      height="64"
+      viewBox="0 0 16.933 16.933"
+    >
+      <g transform="translate(-28.492 -43.537) scale(1.50898)">
+        <path
+          fill="#ff5959"
+          stroke="#000"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="0.39"
+          d="M29.59 36.36c0 2.361-2.587 3.426-5.07 3.434-2.539.008-5.303-1.16-5.115-3.654.112-1.492.653-2.942 1.95-3.78.823-.532 2.196-.757 3.166-.788.906-.029 2.247.138 3.08.71 1.346.926 1.988 2.62 1.988 4.078z"
+          paintOrder="fill markers stroke"
+        ></path>
+        <path
+          fill="#ffd99a"
+          strokeWidth="0.39"
+          d="M22.588 35.72s.607-.79 1.24-.867c.634-.077 1.239-.103 1.633.078.394.18.99.732.99.732l.868.566s.174.302.146.667c-.027.366-.11.777-.557 1.097-.448.32-1.133.801-2.115.904-.982.104-2.312-.53-2.635-.865-.323-.336-.724-.698-.595-1.37.13-.672.4-.723.4-.723z"
+        ></path>
+        <rect
+          width="2.222"
+          height="1.105"
+          x="23.394"
+          y="35.553"
+          fill="#8b193a"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="0.407"
+          paintOrder="fill markers stroke"
+          ry="0"
+        ></rect>
+        <ellipse
+          cx="24.511"
+          cy="37.062"
+          fill="#fd4e56"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="0.424"
+          paintOrder="fill markers stroke"
+          rx="1.208"
+          ry="0.824"
+        ></ellipse>
+        <path
+          fill="none"
+          stroke="#000"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="0.39"
+          d="M23.583 37.441c.29.502.623.533.912.538.317.006.729-.16.966-.561.238-.402.395-1.48.066-1.973-.329-.494-.185.084-.464.224-.244.073-.916.052-1.168-.018-.216-.12-.184-.578-.377-.243-.4.81-.284 1.502.065 2.033z"
+        ></path>
+        <path
+          fill="#52875b"
+          strokeWidth="0.39"
+          d="M20.898 30.674l-.607.02.665 1.55 1.525.323 1.193-.631-.6-1.017z"
+        ></path>
+        <path
+          fill="#a8c930"
+          strokeWidth="0.39"
+          d="M20.689 30.8s.517.867 1.124.944c.607.078.866 0 .97-.026.102-.026.245-.18.219-.297-.026-.116-.22-.18-.233-.245-.013-.065.005-.13.064-.148.06-.018.416.174.416.174l.01-.79-1.092-.19z"
+        ></path>
+        <path
+          fill="#52875b"
+          strokeWidth="0.39"
+          d="M28.121 30.674l.607.02-.665 1.55-1.525.323-1.193-.631.599-1.017z"
+        ></path>
+        <path
+          fill="#a8c930"
+          strokeWidth="0.39"
+          d="M28.33 30.8s-.517.867-1.124.944c-.607.078-.866 0-.97-.026-.103-.026-.245-.18-.219-.297.026-.116.22-.18.233-.245.013-.065-.005-.13-.065-.148-.059-.018-.415.174-.415.174l-.01-.79 1.092-.19z"
+        ></path>
+        <path
+          fill="#52875b"
+          strokeWidth="0.39"
+          d="M24.483 29.193l-1.104 1.086-.028 1.27 1.132.944 1.125-.971.064-1.252z"
+        ></path>
+        <path
+          fill="#a8c930"
+          strokeWidth="0.39"
+          d="M24.865 29.6s.32.539.329.886c.009.347-.046.503-.128.594a.717.717 0 01-.521.238c-.256 0-.466-.074-.566-.156-.101-.082-.314-.301-.305-.575.01-.274.037-.416.082-.517.046-.1.119-.301.119-.301l.608-.576z"
+        ></path>
+        <path
+          fill="none"
+          stroke="#000"
+          strokeWidth="0.39"
+          d="M23.269 30.891c-.026-1.033 1.214-1.73 1.214-1.73s1.24.697 1.215 1.73c-.026 1.034-1.215 1.602-1.215 1.602s-1.188-.568-1.214-1.602z"
+        ></path>
+        <path
+          fill="none"
+          stroke="#000"
+          strokeWidth="0.39"
+          d="M23.345 30.474s-.512-.256-1.041-.219c-.53.037-1.352.53-1.553.457-.201-.073-.384-.292-.42-.183-.037.11-.214.596.512 1.443.381.444 1.206.64 1.663.548.457-.091 1.297-.594 1.297-.594m1.871-1.452s.512-.256 1.041-.219c.53.037 1.352.53 1.553.457.201-.073.384-.292.42-.183.037.11.214.596-.512 1.443-.381.444-1.206.64-1.663.548-.457-.091-1.297-.594-1.297-.594"
+        ></path>
+        <circle
+          cx="21.88"
+          cy="35.441"
+          r="0.807"
+          fill="#41090a"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="0.42"
+          paintOrder="fill markers stroke"
+        ></circle>
+        <ellipse
+          cx="22.27"
+          cy="35.071"
+          fill="#fff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="0.39"
+          paintOrder="fill markers stroke"
+          rx="0.271"
+          ry="0.265"
+        ></ellipse>
+        <circle
+          cx="27.19"
+          cy="35.428"
+          r="0.807"
+          fill="#41090a"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="0.42"
+          paintOrder="fill markers stroke"
+        ></circle>
+        <ellipse
+          cx="26.83"
+          cy="35.071"
+          fill="#fff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="0.47"
+          paintOrder="fill markers stroke"
+          rx="0.273"
+          ry="0.265"
+        ></ellipse>
+        <ellipse
+          cx="44.145"
+          cy="7.838"
+          fill="#fd807a"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="0.228"
+          paintOrder="fill markers stroke"
+          rx="0.382"
+          ry="0.445"
+          transform="matrix(.69374 .72023 -.75145 .6598 0 0)"
+        ></ellipse>
+        <ellipse
+          cx="11.778"
+          cy="43.18"
+          fill="#fd807a"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="0.228"
+          paintOrder="fill markers stroke"
+          rx="0.382"
+          ry="0.445"
+          transform="matrix(-.69374 .72023 .75145 .6598 0 0)"
+        ></ellipse>
+        <ellipse
+          cx="38.196"
+          cy="12.805"
+          fill="#f6fbce"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="0.447"
+          paintOrder="fill markers stroke"
+          rx="0.262"
+          ry="0.368"
+          transform="matrix(.78871 .61476 -.60114 .79915 0 0)"
+        ></ellipse>
+        <ellipse
+          cx="37.087"
+          cy="14.161"
+          fill="#f6fbce"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="0.447"
+          paintOrder="fill markers stroke"
+          rx="0.262"
+          ry="0.368"
+          transform="matrix(.78871 .61476 -.60114 .79915 0 0)"
+        ></ellipse>
+        <ellipse
+          cx="31.932"
+          cy="21.474"
+          fill="#f6fbce"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="0.338"
+          paintOrder="fill markers stroke"
+          rx="0.194"
+          ry="0.284"
+          transform="matrix(.8735 .48684 -.34209 .93967 0 0)"
+        ></ellipse>
+        <ellipse
+          cx="39.15"
+          cy="6.179"
+          fill="#f6fbce"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="0.312"
+          paintOrder="fill markers stroke"
+          rx="0.182"
+          ry="0.257"
+          transform="matrix(.66846 .74375 -.73217 .68112 0 0)"
+        ></ellipse>
+        <ellipse
+          cx="24.465"
+          cy="33.117"
+          fill="#f6fbce"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="0.366"
+          paintOrder="fill markers stroke"
+          rx="0.23"
+          ry="0.281"
+          transform="matrix(.9999 .01414 .00105 1 0 0)"
+        ></ellipse>
+        <ellipse
+          cx="35.53"
+          cy="21.052"
+          fill="#f6fbce"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="0.366"
+          paintOrder="fill markers stroke"
+          rx="0.23"
+          ry="0.281"
+          transform="matrix(.90886 .4171 -.40324 .9151 0 0)"
+        ></ellipse>
+        <ellipse
+          cx="34.803"
+          cy="20.455"
+          fill="#f6fbce"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="0.366"
+          paintOrder="fill markers stroke"
+          rx="0.23"
+          ry="0.281"
+          transform="matrix(.90886 .4171 -.40324 .9151 0 0)"
+        ></ellipse>
+        <ellipse
+          cx="9.106"
+          cy="40.965"
+          fill="#f6fbce"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="0.366"
+          paintOrder="fill markers stroke"
+          rx="0.23"
+          ry="0.281"
+          transform="matrix(.91152 -.41127 .42507 .90516 0 0)"
+        ></ellipse>
+        <ellipse
+          cx="8.777"
+          cy="42.208"
+          fill="#f6fbce"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="0.366"
+          paintOrder="fill markers stroke"
+          rx="0.23"
+          ry="0.281"
+          transform="matrix(.91152 -.41127 .42507 .90516 0 0)"
+        ></ellipse>
+        <ellipse
+          cx="6.002"
+          cy="42.627"
+          fill="#f6fbce"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="0.46"
+          paintOrder="fill markers stroke"
+          rx="0.284"
+          ry="0.359"
+          transform="matrix(.85383 -.52056 .50314 .8642 0 0)"
+        ></ellipse>
+        <ellipse
+          cx="-1.946"
+          cy="44.19"
+          fill="#f6fbce"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="0.431"
+          paintOrder="fill markers stroke"
+          rx="0.266"
+          ry="0.336"
+          transform="matrix(.7157 -.69842 .67097 .74148 0 0)"
+        ></ellipse>
+        <ellipse
+          cx="15.494"
+          cy="43.165"
+          fill="#f6fbce"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="0.296"
+          paintOrder="fill markers stroke"
+          rx="0.183"
+          ry="0.231"
+          transform="matrix(.93073 -.3657 .3304 .94384 0 0)"
+        ></ellipse>
+        <ellipse
+          cx="15.061"
+          cy="43.662"
+          fill="#f6fbce"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="0.348"
+          paintOrder="fill markers stroke"
+          rx="0.214"
+          ry="0.271"
+          transform="matrix(.93073 -.3657 .3304 .94384 0 0)"
+        ></ellipse>
+        <ellipse
+          cx="7.025"
+          cy="41.762"
+          fill="#f6fbce"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="0.447"
+          paintOrder="fill markers stroke"
+          rx="0.262"
+          ry="0.368"
+          transform="matrix(.94948 -.31383 .33007 .94396 0 0)"
+        ></ellipse>
+        <ellipse
+          cx="-13.089"
+          cy="44.705"
+          fill="#f6fbce"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="0.338"
+          paintOrder="fill markers stroke"
+          rx="0.194"
+          ry="0.284"
+          transform="matrix(.79482 -.60685 .72526 .68848 0 0)"
+        ></ellipse>
+        <ellipse
+          cx="-45.142"
+          cy="-12.006"
+          fill="#f6fbce"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="0.447"
+          paintOrder="fill markers stroke"
+          rx="0.262"
+          ry="0.368"
+          transform="matrix(-.78871 -.61476 .60114 -.79915 0 0)"
+        ></ellipse>
+        <ellipse
+          cx="42.712"
+          cy="-19.055"
+          fill="#f6fbce"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="0.46"
+          paintOrder="fill markers stroke"
+          rx="0.284"
+          ry="0.359"
+          transform="matrix(.85383 .52056 .50314 -.8642 0 0)"
+        ></ellipse>
+      </g>
+    </svg>
+  );
+}
