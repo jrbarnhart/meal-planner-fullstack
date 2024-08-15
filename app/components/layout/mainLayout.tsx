91613501bc115e42eab05da36d3e64a5280b57e7
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-08-15 18:48:45 -0400

    Add simple tab nav including add recipe link

--- app/components/layout/mainLayout.tsx
+++ app/components/layout/mainLayout.tsx
@@ -15,7 +15,7 @@ import FavIcon from "../icons/favIcon";
 function InfoHeader({ username }: { username?: string }) {
   const [open, setOpen] = useState(false);
   return (
-    <div className="px-4 py-2 shadow-md flex justify-between items-center">
+    <div className="px-4 py-2 shadow-md md:shadow-none flex justify-between items-center">
       <Link to={"/"}>
         <FavIcon className="size-10" />
       </Link>
@@ -50,14 +50,14 @@ function InfoHeader({ username }: { username?: string }) {
   );
 }
 
-function Nav() {
+function MobileNav() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   const { pathname } = useLocation();
 
   return (
     <Card
-      className={`absolute bottom-1/4 left-0 p-2 bg-card transition-all duration-300 ease-in-out rounded-l-none ${
+      className={`absolute md:hidden bottom-1/4 left-0 p-2 bg-card transition-all duration-300 ease-in-out rounded-l-none ${
         isOpen
           ? "w-48"
           : "w-20 shadow-none border-transparent bg-transparent opacity-55"
@@ -114,6 +114,64 @@ function Nav() {
   );
 }
 
+function Nav() {
+  const { pathname } = useLocation();
+
+  return (
+    <nav className="items-center hidden md:flex w-full justify-center">
+      <div aria-hidden className="shadow-md flex-grow border-b-1 h-full" />
+      <Button
+        variant={pathname === "/meals" ? "ghost" : "default"}
+        className={`rounded-br-none rounded-bl-none border border-transparent shadow-md hover:bg-background hover:text-foreground hover:border-primary hover:border-b-transparent ${
+          pathname === "/meals"
+            ? "border-primary border-b-transparent shadow-none"
+            : ""
+        }`}
+        asChild
+      >
+        <Link to={"/meals"}>Meals</Link>
+      </Button>
+      <div aria-hidden className="shadow-md w-2 border-b-1 h-full" />
+      <Button
+        variant={pathname === "/recipes" ? "ghost" : "default"}
+        className={`rounded-br-none rounded-bl-none border border-transparent shadow-md hover:bg-background hover:text-foreground hover:border-primary hover:border-b-transparent ${
+          pathname === "/recipes"
+            ? "border-primary border-b-transparent shadow-none"
+            : ""
+        }`}
+        asChild
+      >
+        <Link to={"/recipes"}>Recipes</Link>
+      </Button>
+      <div aria-hidden className="shadow-md w-2 border-b-1 h-full" />
+      <Button
+        variant={pathname === "/recipes/library" ? "ghost" : "default"}
+        className={`rounded-br-none rounded-bl-none border border-transparent shadow-md hover:bg-background hover:text-foreground hover:border-primary hover:border-b-transparent ${
+          pathname === "/recipes/library"
+            ? "border-primary border-b-transparent shadow-none"
+            : ""
+        }`}
+        asChild
+      >
+        <Link to={"/recipes/library"}>Recipe Library</Link>
+      </Button>
+      <div aria-hidden className="shadow-md w-2 border-b-1 h-full" />
+      <Button
+        variant={pathname === "/recipes/add" ? "ghost" : "default"}
+        className={`rounded-br-none rounded-bl-none border border-transparent shadow-md hover:bg-background hover:text-foreground hover:border-primary hover:border-b-transparent ${
+          pathname === "/recipes/add"
+            ? "border-primary border-b-transparent shadow-none"
+            : ""
+        }`}
+        asChild
+      >
+        <Link to={"/recipes/add"}>Custom Recipe</Link>
+      </Button>
+      <div aria-hidden className="shadow-md flex-grow border-b-1 h-full" />
+    </nav>
+  );
+}
+
 export default function MainLayout({
   children,
   username,
@@ -123,12 +181,15 @@ export default function MainLayout({
 }) {
   const { pathname } = useLocation();
   return (
-    <div className="relative h-full grid grid-rows-[min-content_1fr] grid-cols-1 overflow-hidden">
+    <div className="relative h-full grid grid-rows-[min-content_1fr] md:grid-rows-[min-content_min-content_1fr] grid-cols-1 overflow-hidden">
       {pathname !== "/" && pathname !== "/signup" ? (
-        <InfoHeader username={username} />
+        <>
+          <InfoHeader username={username} />
+          <Nav />
+        </>
       ) : null}
       {children}
-      {pathname !== "/" && pathname !== "/signup" ? <Nav /> : null}
+      {pathname !== "/" && pathname !== "/signup" ? <MobileNav /> : null}
     </div>
   );
 }
