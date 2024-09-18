c0154e2f978448df863a243a3174861631e921be
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-09-18 02:17:47 -0400

    Adjust config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { vitePlugin as remix } from "@remix-run/dev";
+import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+installGlobals();
+
 export default defineConfig({
+  server: { port: 4000 },
+  base: process.env.NODE_ENV === "production" ? "/meal-planner/" : undefined,
   plugins: [
     remix({
       basename: "/meal-planner",
