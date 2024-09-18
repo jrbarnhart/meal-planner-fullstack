424f019a53f9077e139493ef32a24a1658881379
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-09-17 22:38:28 -0400

    Remove vite config for base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: "/meal-planner/",
   plugins: [
     remix({
       future: {
