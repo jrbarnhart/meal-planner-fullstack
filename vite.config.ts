ab37012a80f9c0b738b83e8cb3f3582a033ae328
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-09-18 00:21:53 -0400

    Again

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/meal-planner/" : undefined,
   plugins: [
     remix({
       future: {
